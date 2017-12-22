68e0b28a3166c7a0ddfbfdb92d0e531dce7d93b7
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2017-12-22 15:05:23 -0600

    added missing files

---
+++ src/demo.js
@@ -0,0 +1,48 @@
+const MySqlConnection = require('./index');
+const mysql = new MySqlConnection({
+  //debug: true,
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'demo'
+});
+
+async function doIt() {
+  const tableName = 'demo_user';
+
+  try {
+    await mysql.deleteAll(tableName);
+
+    const id1 = await mysql.insert(
+      tableName, {username: 'batman', password: 'robin'});
+    console.log('id1 =', id1);
+
+    const id2 = await mysql.insert(
+      tableName, {username: 'joker', password: 'penguin'});
+    console.log('id2 =', id2);
+
+    let result = await mysql.getAll(tableName);
+    console.log('all =', result);
+
+    result = await mysql.updateById(
+      tableName, id1, {username: 'batman', password: 'wayne'});
+
+    result = await mysql.getById(tableName, id1);
+    console.log('just id1 after update =', result);
+
+    const sql = `select * from ${tableName} where password = ?`;
+    result = await mysql.query(sql, 'wayne');
+    console.log('query result =', result);
+
+    await mysql.deleteById(tableName, id1);
+
+    result = await mysql.getAll(tableName);
+    console.log('after deleting id1 =', result);
+  } catch (e) {
+    console.error(e);
+  } finally {
+    mysql.disconnect();
+  }
+}
+
+doIt();
